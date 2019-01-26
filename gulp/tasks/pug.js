7c4405692cd1876e00dd921aee834438e608feea
加藤北斗 <hokuto15@gmail.com>
2019-01-26 19:11:27 +0900

    不要なモジュールを整理

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,8 +1,6 @@
 import gulp from 'gulp'
 import pug from 'gulp-pug'
 import plumber from 'gulp-plumber'
-import vinylYamlData from 'vinyl-yaml-data'
-import deepExtend from 'deep-extend-stream'
 import pugInheritance from 'gulp-pug-inheritance'
 import cached from 'gulp-cached'
 import changed from 'gulp-changed'
@@ -11,22 +9,11 @@ import gulpif from 'gulp-if'
 import browserSync from "browser-sync";
 import paths from '../config'
 
-let locals = {};
-
-gulp.task('yaml', () => {
-	locals = {};
-	return gulp.src(paths.yaml_src)
-	.pipe(plumber())
-	.pipe(vinylYamlData())
-	.pipe(deepExtend(locals))
-});
-
-gulp.task('pugAll', ['yaml'], () => {
-	return gulp.src([paths.pug_src, paths.pug_exclude])
+gulp.task('pugAll', () => {
+	return gulp.src(paths.pug_src)
 	.pipe(plumber())
 	.pipe(pug({
 		pretty: true,
-		locals: locals
 	}))
 	.on('error', (err) => {
 		console.log(err.message);
@@ -34,7 +21,7 @@ gulp.task('pugAll', ['yaml'], () => {
 	.pipe(gulp.dest(paths.dest))
 });
 
-gulp.task('pug', ['yaml'], () => {
+gulp.task('pug', () => {
 	return gulp.src(paths.pug_src)
 	.pipe(plumber())
 	.pipe(changed('dest', {extension: '.html'}))
@@ -48,7 +35,6 @@ gulp.task('pug', ['yaml'], () => {
 	}))
 	.pipe(pug({
 		pretty: true,
-		locals: locals
 	}))
 	.on('error', (err) => {
 		console.log(err.message);
