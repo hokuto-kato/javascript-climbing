c4c8a9c331cb53b5ebd9373d15387cb9d2d98115
加藤北斗 <hokuto15@gmail.com>
2019-02-26 14:54:33 +0900

    observer

--- src/js/util/observer.js
+++ src/js/util/observer.js
@@ -1,10 +1,11 @@
 export default function() {
+
 	//Observerのコンストラクタを作成します
 	function Observer() {
 		//監視者を格納するための空の配列this.listnersを作成します
 		this.listners = [];
 	}
-	
+
 	// 次にon、offというプロトタイプ関数を作成します。
 	// onメソッドはthis.listnersにイベントを通知したい関数を追加します。
 	Observer.prototype.on = function(func) {
@@ -12,6 +13,29 @@ export default function() {
 	};
 	//offメソッドは指定されたオブサーバーを検索しリストから削除します。
 	Observer.prototype.off = function(func) {
-		
+		const len = this.listner.length;
+		for(let i = 0; i < len; i++) {
+			const listner = this.listners[i];
+			if(listner === func) {
+				this.listeners.splice(i, 1);
+			}
+		}
 	};
+
+	//最後にtriggerというプロトタイプ関数を作成します。
+	//triggerメソッドはオブザーバーのリスト全体を反復処理し、実行します。
+	Observer.prototype.trigger = function(event) {
+		const len = this.listners.length;
+		for(let i = 0; i < len; i++) {
+			const listner = this.listners[i];
+			listner();
+		}
+	};
+	
+	const observer = new Observer();
+		const greet = () => {
+			console.log("good morning");
+		};
+		observer.on(greet);
+		observer.trigger();
 }
