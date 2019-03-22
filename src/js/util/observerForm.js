2a8ae5822c6c70c75ffec379017d6f767a4fae35
加藤北斗 <hokuto15@gmail.com>
2019-03-22 23:48:07 +0900

    observerパターンを使ったフォームのバリデート

---
+++ src/js/util/observerForm.js
@@ -0,0 +1,89 @@
+class ValidatorView {
+  constructor() {
+    this.model = new ValidatorModel()
+    this.$title = $('.title')
+    this.$body = $('.body')
+    this.addListeners()
+  }
+  
+  addListeners() {
+    var self = this;
+    this.$title.on('keyup', function(e) {
+      self.model.setTitle($(e.currentTarget).val()) 
+    })
+    
+    this.$body.on('keyup', function(e) {
+      self.model.setBody($(e.currentTarget).val())
+    })
+    
+    this.model.addValidateListener(this.displayErrors)
+  }
+  
+  displayErrors() {
+    $('.errors').text('')
+    
+    var errors = this.errors,
+        errorsStr = ''
+    
+    if (errors.length === 0) return
+    
+    for(var i = 0; i < errors.length; i++) {
+      errorsStr += '<li>' + errors[i] + '</li>' 
+    }
+    
+    $('.errors').append(errorsStr);
+  }
+  
+}
+  
+
+class ValidatorModel {
+  constructor() {
+    this.title = ''
+    this.titleMinLength = 1
+    this.titleMaxLength = 10
+    this.body = ''
+    this.bodyMinLength = 1
+    this.bodyMaxLength = 30
+    this.errors = []
+    this.validateListener = function() {}
+  }
+  
+  setTitle(title) {
+    this.title = title;
+    this.valid();
+  }
+  
+  setBody(body) {
+    this.body = body;
+    this.valid();
+  }
+  
+  addValidateListener(listener) {
+    this.validateListener = listener
+  }
+  
+  valid() {
+    this.errors = [];
+    
+    if (this.title.length < this.titleMinLength) {
+      this.errors.push('タイトルは' + this.titleMinLength + '文字以上でおなしゃす')
+    }
+    
+    if (this.title.length > this.titleMaxLength) {
+      this.errors.push('タイトルは' + this.titleMaxLength + '文字を超えています')
+    }
+    
+    if (this.body.length < this.bodyMinLength) {
+      this.errors.push('本文は' + this.bodyMinLength + '文字以上でおなしゃす')
+    }
+    
+    if (this.body.length > this.bodyMaxLength) {
+      this.errors.push('本文は' + this.bodyMaxLength + '文字を超えています')
+    }
+    
+    this.validateListener();
+  }
+}
+
+new ValidatorView();
